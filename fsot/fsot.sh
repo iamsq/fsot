7bdce45243d86a8982761f1dea70ee70c560d0cf
SQ <shi.ng.10@ucl.ac.uk>
2013-02-04 19:38:27 +0000

    Included Mock test lib jars, added shell script

--- fsot/fsot.sh
+++ fsot/fsot.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-EXEC="$0"
+EXEC="`dirname $0`"
 POS="${EXEC%/*}"
+CODECOVER_DIR="$POS/codecover"
+antCodeCoverXML="$POS/ant-codecover.xml"
 
 exec java -Xmx512M -jar "$POS/fsot.jar" "$@"
