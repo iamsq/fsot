3bb4364aa4c32226e75f40ce7fdb7e7c72bcfc62
iamsq <shi.ng.10@ucl.ac.uk>
2013-02-04 19:59:56 +0000

    Update fsot/fsot.sh
    Edited shell script works on Mac!

--- fsot/fsot.sh
+++ fsot/fsot.sh
@@ -2,7 +2,7 @@
 
 EXEC="`dirname $0`"
 POS="${EXEC%/*}"
-CODECOVER_DIR="$POS/codecover"
-antCodeCoverXML="$POS/ant-codecover.xml"
+export CODECOVER_DIR="$POS/codecover"
+export antCodeCoverXML="$POS/ant-codecover.xml"
 
-exec java -Xmx512M -jar "$POS/fsot.jar" "$@"
+java -Xmx512M -jar "$POS/fsot.jar" "$@"
